4e22ac2a5c6e66bf615ddd759bcd8f6c049a6502
ZX-Mercury <laohuangnextdoor@outlook.com>
2024-03-18 21:19:02 +0800

    初步实现基本功能。

--- priority_queue/src/priority_queue.hpp
+++ priority_queue/src/priority_queue.hpp
@@ -10,66 +10,196 @@ namespace sjtu {
 /**
  * a container like std::priority_queue which is a heap internal.
  */
-template<typename T, class Compare = std::less<T>>
-class priority_queue {
-public:
-	/**
-	 * TODO constructors
-	 */
-	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
+    template<typename T, class Compare = std::less<T>>
+    class priority_queue {
+    private:
+        struct node {
+            T *data;
+            node *left, *right;
+            int npl;
 
-	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
+            node() {
+                data = new T;
+                left = nullptr;
+                right = nullptr;
+                npl = 0;
+            }
 
-	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	void pop() {
+            node(const T &e) {
+                data = new T(e);
+                left = nullptr;
+                right = nullptr;
+                npl = 0;
+            }
 
-	}
-	/**
-	 * return the number of the elements.
-	 */
-	size_t size() const {
+            ~node() {
+                delete data;
+                data = nullptr;
+                left = nullptr;
+                right = nullptr;
+                npl = 0;
+            }
 
-	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
-	bool empty() const {
+            bool operator<(const node &other) {
+                return !(*data < *(other.data));
+            }
+        };
 
-	}
-	/**
-	 * merge two priority_queues with at most O(logn) complexity.
-	 * clear the other priority_queue.
-	 */
-	void merge(priority_queue &other) {
+        node *root;
+        size_t size1;
 
-	}
-};
+        node *merge(node *l, node *r) {
+            if (l == nullptr) return r;
+            if (r == nullptr) return l;
+            if (*l < *r)return merge1(l, r);
+            else return merge1(r, l);
+        }
+
+        void swapChildren(node *t) {
+            node *tmp;
+            tmp = t->left;
+            t->left = t->right;
+            t->right = tmp;
+        }
+
+
+        node *merge1(node *l, node *r) {
+            if (l->left == nullptr) { l->left = r; }
+            else {
+                l->right = merge(l->right, r);
+                if (l->left->npl < l->right->npl)
+                    swapChildren(l);
+                l->npl = l->right->npl + 1;
+            }
+            return l;
+        }
+
+        node *build(node *t, node *other) {
+            if (other == nullptr) {
+                return nullptr;
+            }
+            t = new node;
+            *(t->data) = *(other->data);
+            t->npl = other->npl;
+            t->left = build(t->left, other->left);
+            t->right = build(t->right, other->right);
+            return t;
+        }
+
+        /*void Copy(BiTree t,BiTree &newt){
+            if(t==NULL){
+                newt = NULL;
+                return;
+            }
+            else{
+                newt = new BiTNode;
+                newt->data = t->data;
+                Copy(t->lchild,newt->lchild);
+                Copy(t->rchild,newt->rchild);
+            }
+        }*/
+
+        void clear(node *t) {
+            if (t == nullptr) return;
+            clear(t->left);
+            clear(t->right);
+            delete t;
+            t = nullptr;
+        }
+
+    public:
+        /**
+         * TODO constructors
+         */
+        priority_queue() {
+            root = nullptr;
+            size1 = 0;
+        }
+
+        priority_queue(const priority_queue &other) {
+            root = build(root, other.root);
+            size1 = other.size1;
+        }
+
+        /**
+         * TODO deconstructor
+         */
+        ~priority_queue() {
+            clear(root);
+            root = nullptr;
+            size1 = 0;
+        }
+
+        /**
+         * TODO Assignment operator
+         */
+        priority_queue &operator=(const priority_queue &other) {
+            clear(root);
+            root= build(root,other.root);
+            size1=other.size1;
+            return *this;
+            //root
+        }
+
+        /**
+         * get the top of the queue.
+         * @return a reference of the top element.
+         * throw container_is_empty if empty() returns true;
+         */
+        const T &top() const {
+            if (empty()) { throw container_is_empty(); }
+            return *(root->data);
+        }
+
+        /**
+         * TODO
+         * push new element to the priority queue.
+         */
+        void push(const T &e) {
+            auto p1 = new node(e);
+            root = merge(p1, root);
+            size1++;
+        }
+
+        /**
+         * TODO
+         * delete the top element.
+         * throw container_is_empty if empty() returns true;
+         */
+        void pop() {
+            if (empty()) throw container_is_empty();
+            auto tmp = root;
+            root = merge(root->left, root->right);
+            delete tmp;
+            size1--;
+        }
+
+        /**
+         * return the number of the elements.
+         */
+        size_t size() const {
+            return size1;
+        }
+
+        /**
+         * check if the container has at least an element.
+         * @return true if it is empty, false if it has at least an element.
+         */
+        bool empty() const {
+            return root == nullptr;
+        }
+
+        /**
+         * merge two priority_queues with at most O(logn) complexity.
+         * clear the other priority_queue.
+         */
+        void merge(priority_queue &other) {
+            if (this == &other) return;
+            merge(root, other.root);
+            other.root = nullptr;
+            size1 += other.size1;
+        }
+    };
 
 }
 
